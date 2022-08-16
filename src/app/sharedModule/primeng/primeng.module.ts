04745c94fa7b01893ee7da693d7a3d45e4e833fb
Tirth <bhalanitirth999@gmail.com>
2022-08-16 22:37:32 +0530

    initial commite

--- src/app/sharedModule/primeng/primeng.module.ts
+++ src/app/sharedModule/primeng/primeng.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ButtonModule} from 'primeng/button';
 import {MultiSelectModule} from 'primeng/multiselect';
-
+import {TabViewModule} from 'primeng/tabview';
 
 @NgModule({
   declarations: [], 
@@ -12,6 +12,7 @@ import {MultiSelectModule} from 'primeng/multiselect';
   exports:[
     ButtonModule,
     MultiSelectModule,
+    TabViewModule,
 
   ]
 })
